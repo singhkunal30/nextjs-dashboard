e10bcd0433d01d0e3aae1c2377f823ebddbc28d4
Kunal Singh <singhkunal30@outlook.com>
2025-10-17 23:28:14 +0530

    comment loading.tsx

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,4 +1,4 @@
-export default function Loading() {
-  // Add fallback UI that will be shown while the route is loading.
-  return <LoadingSkeleton />
-}
+// export default function Loading() {
+//   // Add fallback UI that will be shown while the route is loading.
+//   return <LoadingSkeleton />
+// }
